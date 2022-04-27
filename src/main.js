bcd0aabdcbfdd577595fb1f6e623d4cb807dd94e
camicamposc <clearca57@gmail.com>
2022-04-27 16:14:10 -0400

    add screen welcome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+// import { example } from './data.js';
+// // import data from './data/lol/lol.js';
+// import data from './data/pokemon/pokemon.js';
+// // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+// console.log(example, data);
+
+const btnNo = document.getElementById("no");
+btnNo.addEventListener("click", ()=>{
+    alert("You will be redirected to google");
+    window.location.assign("https://www.google.com");
+});
+
+const btnYes = document.getElementById("yes");
+btnYes.addEventListener("click", () =>{
+    window.location.href="./welcome.html"
+})
